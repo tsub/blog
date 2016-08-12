b3b78df24170b5142f96c7be4331fb5126b8f8c0
tsub <tsubasatakayama511@gmail.com>
2016-08-12 16:15:49 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,11 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+# Clean directory
+if [ -e "./public" ]; then
+  rm -rf public
+fi
+
 # Clone github-pages repository.
 git clone git@github.com:tsub/tsub.github.io.git public
 
